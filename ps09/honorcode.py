9fab69a0126fc3aeb75526c699e3132d527d8005
anixshi <48997234+anixshi@users.noreply.github.com>
2019-07-28 13:19:10 -0400

    Add files via upload

---
+++ ps09/honorcode.py
@@ -0,0 +1,37 @@
+"""
+By submitting this assignment,
+I certify that our code is entirely written by me (and my partner for partner tasks),
+adhering to Wellesley's Honor Code.
+"""
+
+# Replace the empty strings with the appropriate values before you submit.
+# Your username is your Wellesley e-mail username (not including @wellesley.edu)
+
+YourFullName = "Anika Shields"
+YourUsername = "ashields"
+
+PartnerFullName = "None" # If you did not have a partner in Task3, write "None"
+PartnerUsername = "None" # If you did not have a partner in Task3, write "None"
+
+TimeReading = 1 # time in hours spent on general reading
+TimeTask1 = 2 # time in hours spent on Task 1
+TimeTask2 = 1 # time in hours spent on Task 2
+TimeTask3 = 1  # time in hours spent on task 3
+
+# Adhering to Wellesley's Honor Code, how did you write your code?
+# Enter 1, 2, 3, or 4 for HowWeWorked:
+# 1. I (or we, if you had a partner) talked exclusively to each other and used the 111 website, with no external help
+# 2. I (or we, if you had a partner) received help from CS111 Staff (name people in "Sources")
+# 3. I (or we, if you had a partner) consulted external sources. (name in "Sources")
+# 4. Both 2 and 3: I (or we, if you had a partner) received help from staff and consulted external sources. (name in "Sources")
+HowWeWorked = 2
+
+# If you received help from CS111 Staff OR consulted external sources,
+# list the people or sources below; otherwise, leave empty.
+Sources = "Helproom, Sohie"
+
+# If you need to explain any special situation, use the Notes string
+# below; otherwise, leave empty. For example, if you started collaboraring
+# with a partner but for some reason could not complete that collaboration, 
+# explain that here. 
+Notes = ""
